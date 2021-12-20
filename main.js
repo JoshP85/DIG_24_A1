e42f6eea955dc58eb9ed6d7391c7a09a404a1592
s3655612 <s3655612@student.rmit.edu.au>
2021-12-20 17:35:25 +1100

    removed duplicate IDs, <a> tags from buttons

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-// document.querySelector(".start").addEventListener("click", function() {
-//     console.log("Clicked");
-//   });
-
-// Navigation functionality for menu - When a menu item is clicked, this function will get the id of the menu item that 
-// triggered the event, it will then loop through all divs with the class name of 'div-item'. All divs that do not
-// have an id matching the event will have their class names appended with 'display-none' which will hide them from view.
-// Once a matching id is found, the corresponding class name of the div will have 'display-none' removed 
-// and the div will be shown.
-// With help from https://stackoverflow.com/questions/55603456/a-simple-way-to-show-one-div-and-hide-all-others
-window.onload = function () {
-  const menuItems = document.querySelectorAll('.menu-item');
+function showDiv(div) {
   const divItems = document.querySelectorAll('.div-item');
 
-  const setDisplay = function (event) {
-    divItems.forEach(function (div) {
-      if (event.target.getAttribute('id') != div.getAttribute('id')) div.classList.add('display-none');
-      else div.classList.remove('display-none');
-    });
+for(var x = 0; x<divItems.length; x++){
+  if(divItems[x].id != div){
+    divItems[x].classList.add('display-none');
+  }else{
+    divItems[x].classList.remove('display-none');
   }
-
-  menuItems.forEach(function (div) {
-    div.onclick = setDisplay;
-  });
+}
 }
 
 function calculate() {
